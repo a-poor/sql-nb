ef270d85ec0e7e3364e4b1a2e800c1bd65e5753a
a-poor <45295232+a-poor@users.noreply.github.com>
2022-02-11 15:38:08 -0800

    Adding simple code editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,33 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-sql";
+import "prismjs/themes/prism.css"; //Example style, you can use another
+
+
+const starterCode = "SELECT 1 + 1;"
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [code, setCode] = useState(starterCode)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1>Hello, World!</h1>
+      <Editor 
+        value={code}
+        onValueChange={(code) => setCode(code)}
+        highlight={(code) => highlight(code, languages.sql)}
+        padding={10}
+        style={{
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 12,
+          border: '1px solid #ccc',
+        }}
+  
+      />
     </div>
   )
 }
